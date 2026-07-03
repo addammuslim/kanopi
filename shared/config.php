ef31e5b83744d4cf626d9a4561d60cc560510b08
addammuslim <addammuslim@gmail.com>
2026-07-03 20:45:19 +0700

    Add files via upload

---
+++ shared/config.php
@@ -0,0 +1,15 @@
+<?php
+$base_url = "http://" . $_SERVER['HTTP_HOST']."/kanopi";
+if (!empty($_GET['kota'])) {
+	$kota = $_GET['kota'];
+}else{
+	$kota = "Membran";
+}
+$nohp = '6285887639199';
+$notlp = '0858-8763-9199';
+$email = 'rizkiabadicanopy@gmail.com';
+$workshop  = 'Sukabumi - Bogor - Jakarta - Bandung - Depok - Tangerang - Serang - dan sekitarnya';
+$jam_oprasional = '08:00 - 17:00 (Senin - Minggu)';
+$sapa = 'Rizki Abadi Canopy';
+
+?>
